3446496e61247f10dcfd914594de6c9fc45ec7b4
sberkaye <berkayergin99@gmail.com>
2026-03-29 23:29:38 +0300

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Aleo",
+      cssVariable: "--font-aleo",
+      weights: [700],
+      styles: ["normal"],
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Urbanist",
+      cssVariable: "--font-urbanist",
+      weights: [400, 500, 600, 700],
+      styles: ["normal", "italic"],
+    },
+  ],
+});
